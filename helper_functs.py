195c28df7bf945246e827c7eb5ad21c23d4dd46b
Abel Peter <peterabel791@gmail.com>
2022-10-19 23:37:17 +0300

    linked list
    Signed-off-by: Abel Peter <peterabel791@gmail.com>

---
+++ helper_functs.py
@@ -0,0 +1,21 @@
+
+
+
+def even(num, *args):
+    if num%2==0:
+        return "even_number"
+    else:
+        return False, *args
+
+def sick():
+    if even:
+        print(num)
+
+
+
+def abel():
+    pass
+
+
+if __name__ == "__main__":
+   print(even(29))
---
+++ linked_lists.py
@@ -0,0 +1,35 @@
+
+
+
+ls1= [[1,2,3,4,5,6],[-1,-2,-3,-4,-5,-6]]
+
+
+#accessing the first inner list
+def inner_list1():
+    if ls1[0][0]==1:
+     print(True)
+    if ls1[0][1]==2:
+     print(True) 
+    if ls1[0][2]==3:
+     print(True)
+    if ls1[0][3]==4:
+     print(True)  
+    if ls1[0][4]==5:
+     print(True)
+print(inner_list1())
+
+
+
+#accessing the second inner list
+def inner_list2():
+    if ls1[1][0]==-1:
+     print(True)
+    if ls1[1][1]==-2:
+     print(True) 
+    if ls1[1][2]==-3:
+     print(True)
+    if ls1[1][3]==-4:
+     print(True)  
+    if ls1[1][4]==-5:
+     print(True)
+print (inner_list2())
