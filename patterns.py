9211dd64ef2b98ef9e736e2f1ba79a876fe6d652
Abel Peter <peterabel791@gmail.com>
2022-10-24 20:32:04 +0300

    new file
    Signed-off-by: Abel Peter <peterabel791@gmail.com>

---
+++ patterns.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+x = "*"
+
+for j in range(9):  
+    for i in range(j):
+       
+            print(x,end="")
+    
+    print()
+for j in range(8):  
+    for i in range(8-j):
+       
+            print(x,end="")
+            
+    print()
+    
+   
+
